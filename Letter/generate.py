2dac34bbf3eb2cd7e2d878cd041595aeaf71851b
kinda-raffy <rafat.mahiuddin@gmail.com>
2023-09-09 21:14:22 +1000

    feat: add letter generation

---
+++ Letter/generate.py
@@ -0,0 +1,35 @@
+import letter_parser
+import os
+import shutil
+
+
+class Generator:
+    def __init__(self) -> None:
+        self.directory = os.path.join(os.getcwd(), "config")
+        self.prepare_configuration_directory()
+
+    def generate(self, path: os.PathLike) -> None:
+        parser = letter_parser.LetterParser(path)
+        config = parser.parse()
+        self.write_configuration(config)
+
+    def write_configuration(self, config: dict[str, str]) -> None:
+        for key, value in config.items():
+            self.create_tex_file(key, value)
+
+    def create_tex_file(self, name: str, content: str) -> None:
+        with open(os.path.join(self.directory, f"{name}.tex"), "w") as f:
+            f.write(content)
+
+    def prepare_configuration_directory(self) -> None:
+        shutil.rmtree(self.directory, ignore_errors=True)
+        os.makedirs(self.directory)
+
+
+def main():
+    generator = Generator()
+    generator.generate("Company.letter")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Letter/letter_parser.py
@@ -0,0 +1,69 @@
+import os
+import typing
+import itertools
+
+
+class MultiLineSeparator(typing.TypedDict):
+    start: str
+    end: str
+
+class LetterParser:
+    def __init__(self, path: os.PathLike) -> None:
+        self.letter_path = path
+        self.single_line_sep = ":-"
+        self.multi_line_sep = MultiLineSeparator(start="~-", end="-~")
+        self.letter_iterator = iter(self.read_letter_file())
+
+    def parse(self) -> typing.Dict[str, str]:
+        configurations = dict()
+        for line in self.letter_iterator:
+            if self.single_line_sep in line:
+                key, value = self.read_single_line(line)
+            elif self.multi_line_sep["start"] in line:
+                key, value = self.read_multi_line(line)
+            elif line == "\n":
+                continue
+            else:
+                raise ValueError(f"Invalid line: {line}")
+            configurations[key.strip().capitalize()] = value.strip()
+        return configurations
+
+    def read_single_line(self, line: str) -> typing.Tuple[str, str]:
+        return line.split(self.single_line_sep)
+
+    def read_multi_line(self, line: str) -> typing.Tuple[str, str]:
+        name = self.determine_multi_line_name(line, default="Content")
+        return name, self.read_multi_line_content()
+
+    def read_multi_line_content(self, allow_single_newline=True) -> str:
+        content = str()
+        parser, pre_parser = itertools.tee(self.letter_iterator)
+        next(pre_parser, None)
+        for line, next_line in itertools.zip_longest(parser, pre_parser):
+            halt = self.multi_line_sep["end"] in line
+            if halt:
+                break
+            current_line_has_text = line != "\n"
+            next_line_has_text = next_line != "\n" and next_line is not None
+            skip_single_newline = not current_line_has_text and next_line_has_text
+            if allow_single_newline or not skip_single_newline:
+                content += line
+        return content
+
+    def determine_multi_line_name(self, line: str, *, default: str) -> str:
+        name = line.split(self.multi_line_sep["start"])[0]
+        return name if name != "" else default
+
+    def read_letter_file(self) -> typing.List[str]:
+        with open(self.letter_path, "r") as f:
+            return f.readlines()
+
+
+def debug():
+    parser = LetterParser("Example.letter")
+    import pprint
+    pprint.pprint(parser.parse())
+
+
+if __name__ == "__main__":
+    raise RuntimeError("Parser is not meant to be ran directly.")
